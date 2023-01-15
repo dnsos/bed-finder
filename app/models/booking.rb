c3a6f400d81e62532f752840f9dc6e6d9312de0c
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2023-01-15 16:49:02 +0100

    chore: remove bookings resource
    Because we now use the occupancies resource

--- app/models/booking.rb
+++
@@ -1,9 +0,0 @@
-class Booking < ApplicationRecord
-  belongs_to :bed
-
-  validates :checkin_date, :checkout_date, presence: true
-  validates :checkin_date, comparison: { less_than_or_equal_to: :checkout_date }
-  validates :checkin_date, uniqueness: { scope: :bed_id, message: "Can not have two checkins for the same bed on the same day" }
-  validates :checkout_date, uniqueness: { scope: :bed_id, message: "Can not have two checkouts for the same bed on the same day" }
-
-end
