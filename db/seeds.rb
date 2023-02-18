83e1f556964951f1fb57eb773078972fe1ceedd2
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2023-02-18 20:30:49 +0100

    chore: update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,41 @@
-facility_one = Facility.create! name: "Einrichtung Eins", district: "Mitte"
+districts = %w[
+  Mitte
+  Friedrichshain-Kreuzberg
+  Pankow
+  Charlottenburg-Wilmersdorf
+  Spandau
+  Steglitz-Zehlendorf
+  Tempelhof-Schöneberg
+  Neukölln
+  Treptow-Köpenick
+  Marzahn-Hellersdorf
+  Lichtenberg
+  Reinickendorf
+]
 
-facility_two = Facility.create! name: "Einrichtung Zwei", district: "Lichtenberg"
-facility_three = Facility.create! name: "Einrichtung Drei", district: "Tempelhof-Schöneberg"
+districts.each { |district| District.create! name: district }
 
-genders = ["männlich", "weiblich", "divers"]
+facility_one =
+  Facility.create! name: "Einrichtung Eins", district: District.first
+
+facility_two =
+  Facility.create! name: "Einrichtung Zwei", district: District.last
+facility_three =
+  Facility.create! name: "Einrichtung Drei", district: District.first
+
+genders = %w[männlich weiblich divers]
 
 50.times do
-  Bed.create! facility: facility_one, permitted_genders: genders.sample(1 + rand(genders.count))
+  Bed.create! facility: facility_one,
+              permitted_genders: genders.sample(1 + rand(genders.count))
 end
 
 25.times do
-  Bed.create! facility: facility_two, permitted_genders: genders.sample(1 + rand(genders.count))
+  Bed.create! facility: facility_two,
+              permitted_genders: genders.sample(1 + rand(genders.count))
 end
 
 15.times do
-  Bed.create! facility: facility_three, permitted_genders: genders.sample(1 + rand(genders.count))
+  Bed.create! facility: facility_three,
+              permitted_genders: genders.sample(1 + rand(genders.count))
 end
-
